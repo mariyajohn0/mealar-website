3f26df48db3e07313054dc8612c9e248072b9ab8
mariyajohn0 <mariyajohn0104@gmail.com>
2025-12-08 23:21:22 +0530

    vite changes

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mealar-website/',  // <-- your repo name
-  server: {
-    port: 3000
-  }
+  base: '/mealar-website/' // <-- your repo name
+  // server: {
+  //   port: 3000
+  // }
 })
